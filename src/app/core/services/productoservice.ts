329c11daea92a6dbc5ffae5ec2f0304d29362265
shirleyvirtual <shir.copias@gmail.com>
2025-10-27 17:31:57 -0400

    docs: Configuracion para desplegar tabla de productos de la base de datos

---
+++ src/app/core/services/productoservice.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Productoservice {
+  
+  private base = 'http://127.0.0.1:8000/api/productos';
+/**
+ * 
+ */
+constructor(private http:HttpClient){
+
+}
+
+  listaProductos():Observable<any[]> {
+  return this.http.get<any[]>(this.base);
+ }
+
+}
+
